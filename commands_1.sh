2bfec49667604692f14af955a0ecaab984a5517e
chetna <guptachetna96@gmail.com>
2023-06-18 21:04:34 +0530

    output-1

---
+++ commands_1.sh
@@ -0,0 +1,16 @@
+pwd
+ls
+ls -l
+ls -a
+ls -al
+ls -lS
+cd ..
+ls -a > out.txt
+cat out.txt
+cat -n out.txt
+cat -b out.txt
+mkdir test
+mkdir test/img
+mkdir test/img/{a,b,c}
+rmdir test/img/a
+rm -r test
